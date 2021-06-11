23cd2919c7b509a940dccd3f0dff16ee51226873
Tejas <tejas14all@gmail.com>
2021-06-11 09:58:07 +0530

    employee delete - list rest api

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, ObservedValuesFromArray } from 'rxjs';
 import { Employee } from './employee';
 
 @Injectable({
@@ -16,4 +16,18 @@ export class EmployeeService {
     let url = "http://restapi2020.herokuapp.com/api/users.json"
      return this.httpClient.post(url,employee);
   }
+
+  getAllEmployees():Observable<any>{
+    let url = "http://restapi2020.herokuapp.com/api/users.json"
+    return this.httpClient.get(url)
+  }
+
+  deleteEmployeeById(id:number):Observable<any>{
+    let url  = "http://restapi2020.herokuapp.com/api/users/"+id+".json"
+    
+    return this.httpClient.delete(url)
+  }
+
+
+
 }
--- src/app/list-employee/list-employee.component.ts
+++ src/app/list-employee/list-employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-list-employee',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListEmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private employeeService:EmployeeService,private router:Router,private toastr:ToastrService) { }
 
+  employees:Array<Employee>  = [] 
   ngOnInit(): void {
+    this.employeeService.getAllEmployees().subscribe(resp=>{
+        this.employees = resp
+        console.log(resp);
+         
+    })
+  }
+
+  deleteEmployee(id:number){
+    this.employeeService.deleteEmployeeById(id).subscribe(resp=>{
+      this.toastr.success("Employee Deleted","",{timeOut:3000})
+      this.employeeService.getAllEmployees().subscribe(resp=>{
+        this.employees = resp;
+      })
+    })
   }
 
 }
