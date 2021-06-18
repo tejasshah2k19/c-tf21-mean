e2075e10673deb4708ecdb6f9d43be3570b8b7f3
Tejas <tejas14all@gmail.com>
2021-06-18 09:52:09 +0530

    custom validation

--- src/app/reactive-forms-signup/reactive-forms-signup.component.ts
+++ src/app/reactive-forms-signup/reactive-forms-signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms-signup',
@@ -9,14 +9,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ReactiveFormsSignupComponent implements OnInit {
 
 
-  myForm:FormGroup
-  constructor() { 
+  myForm: FormGroup
+  constructor() {
 
     this.myForm = new FormGroup({
-      firstName:new FormControl('',[Validators.required]),
-      email : new FormControl('',[Validators.required,Validators.email]),
-      password : new FormControl('',[Validators.required]),
-      confirmPassword : new FormControl('',[Validators.required]),
+      firstName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, this.passwordStrength]),
+      confirmPassword: new FormControl('', [Validators.required]),
 
     })
 
@@ -25,10 +25,38 @@ export class ReactiveFormsSignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  saveUser(){
-    console.log("save user Called....") 
+  saveUser() {
+    console.log("save user Called....")
     console.log(this.myForm.value)
-    console.log("valid?? ",this.myForm.valid)
+    console.log("valid?? ", this.myForm.valid)
 
   }
+
+  passwordStrength(control: AbstractControl) { //password ==> FormControl
+    let password: string = control.value // extract value of password from control 
+    let count = 0;
+    let isDigit = 0, isUpper = 0, isLower = 0, isSpecial = 0;
+    for (let i = 0; i < password.length; i++) {
+      if (password.charAt(i) >= 'a' && password.charAt(i) <= 'z') {
+        isLower = 1
+      } else if (password.charAt(i) >= 'A' && password.charAt(i) <= 'Z') {
+        isUpper = 1
+      } else if (password.charAt(i) >= '0' && password.charAt(i) <= '9') {
+        isDigit = 1
+      } else {
+        isSpecial = 1
+      }
+      count++
+    }
+
+    if (count < 8 || isDigit == 0 || isLower == 0 || isUpper == 0 || isSpecial == 0) {
+      return {
+        strengthError: true
+      }
+    } else {
+      return null
+    }
+
+  }
+
 }
